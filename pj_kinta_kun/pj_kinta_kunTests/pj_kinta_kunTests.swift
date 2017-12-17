cd2c4a0a7678d82e6e2586a8268ebfbc7bf4e158
mht-taichi-yuuki <t.yuuki@mht-code.com>
2017-12-17 16:20:39 +0900

    first commit

---
+++ pj_kinta_kun/pj_kinta_kunTests/pj_kinta_kunTests.swift
@@ -0,0 +1,36 @@
+//
+//  pj_kinta_kunTests.swift
+//  pj_kinta_kunTests
+//
+//  Created by 行木太一 on 2017/12/17.
+//  Copyright © 2017年 ManhattanCode.Inc. All rights reserved.
+//
+
+import XCTest
+@testable import pj_kinta_kun
+
+class pj_kinta_kunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
