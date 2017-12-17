cc62934093aca0da6f3079ddb49c16836d072c9d
mht-taichi-yuuki <t.yuuki@mht-code.com>
2017-12-17 17:17:05 +0900

    スプラッシュ画面作成

--- pj_kinta_kun/pj_kinta_kun/View/Sprash/SplashViewController.swift
+++ pj_kinta_kun/pj_kinta_kun/View/Sprash/SplashViewController.swift
@@ -8,28 +8,22 @@
 
 import UIKit
 
-class SplashViewController: UIViewController {
+/// スプラッシュ画面
+final class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            if let vc = R.storyboard.main.instantiateInitialViewController() {
+                vc.modalTransitionStyle = .crossDissolve
+                self.present(vc, animated: true, completion: nil)
+            }
+        }
     }
-    */
-
 }
