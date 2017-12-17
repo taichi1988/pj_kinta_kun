88696b1f3a8d3c6134557245a904f396656cbfdf
mht-taichi-yuuki <t.yuuki@mht-code.com>
2017-12-17 17:05:53 +0900

    import R.swift

---
+++ pj_kinta_kun/pj_kinta_kun/View/Sprash/SplashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SplashViewController.swift
+//  pj_kinta_kun
+//
+//  Created by 行木太一 on 2017/12/17.
+//  Copyright © 2017年 ManhattanCode.Inc. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
