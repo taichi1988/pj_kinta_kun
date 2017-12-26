9cc1c1c71f542d321cd29cdb798c9ce1a6263ad7
mht-taichi-yuuki <t.yuuki@mht-code.com>
2017-12-27 00:26:20 +0900

    勤怠履歴（日）画面のヘッダーのレイアウト設定

---
+++ pj_kinta_kun/pj_kinta_kun/View/HistoryDayList/HistoryDayListHeaderView.swift
@@ -0,0 +1,44 @@
+//
+//  HistoryDayListHeaderView.swift
+//  pj_kinta_kun
+//
+//  Created by 行木太一 on 2017/12/27.
+//  Copyright © 2017年 ManhattanCode.Inc. All rights reserved.
+//
+
+import UIKit
+
+class HistoryDayListHeaderView: UIView {
+    let monthLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initLayout() {
+        monthLabel.text = "October"
+        monthLabel.textColor = .primary
+        monthLabel.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+        let underLine = UIView()
+        underLine.backgroundColor = .primary
+        
+        addSubviews(monthLabel, underLine)
+        
+        monthLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(22)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(2)
+        }
+        underLine.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.left.right.equalToSuperview().inset(30)
+            make.height.equalTo(1)
+        }
+    }
+}
--- pj_kinta_kun/pj_kinta_kun/View/HistoryDayList/HistoryDayListViewController.swift
+++ pj_kinta_kun/pj_kinta_kun/View/HistoryDayList/HistoryDayListViewController.swift
@@ -8,28 +8,43 @@
 
 import UIKit
 
-class HistoryDayListViewController: UIViewController {
+final class HistoryDayListViewController: UIViewController {
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        initLayout()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func initLayout() {
+        view.addSubviews(tableView)
+        tableView.backgroundColor = .white
+        tableView.snp.makeConstraintsEqualToSuperview()
     }
-    */
+}
 
+extension HistoryDayListViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = HistoryDayListHeaderView()
+        return headerView
+    }
+}
+
+extension HistoryDayListViewController: UITableViewDelegate {
+    
 }
