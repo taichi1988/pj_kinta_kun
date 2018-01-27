1bb92924623e5e6660bfac2540cd0e4f4709cf6f
mht-taichi-yuuki <t.yuuki@mht-code.com>
2018-01-27 15:45:08 +0900

    save

---
+++ pj_kinta_kun/pj_kinta_kun/View/InitialSetting/InitialSettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InitialSettingViewController.swift
+//  pj_kinta_kun
+//
+//  Created by 行木太一 on 2018/01/21.
+//  Copyright © 2018年 ManhattanCode.Inc. All rights reserved.
+//
+
+import UIKit
+
+final class InitialSettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
